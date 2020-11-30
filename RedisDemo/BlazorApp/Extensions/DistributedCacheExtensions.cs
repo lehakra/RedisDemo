c6d3a1949efc1b7afd09b48d28bf1f79657740fb
Lehakra <57846370+lehakra@users.noreply.github.com>
2020-11-30 17:08:10 +0000

    Added extension methods to talk to IDistributedCache

---
+++ RedisDemo/BlazorApp/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions // not required but you must do this
+            {
+                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+
+            await cache.SetStringAsync(recordId, JsonSerializer.Serialize(data), options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var json = await cache.GetStringAsync(recordId);
+
+            return json is null
+                ? default
+                : JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
